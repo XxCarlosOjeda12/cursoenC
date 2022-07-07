6fb3d445343cf5b4755a3982a5f48684c3692aba
XxCarlosOjeda12 <108698043+XxCarlosOjeda12@users.noreply.github.com>
2022-07-07 17:06:13 -0500

    Add files via upload

---
+++ Clase 4/secEscape3.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+
+// Generamos un triangulo
+
+
+printf("\n\t \t\t     * ");
+printf("\n\t \t\t * \t * ");
+printf("\n\t \t    *  \t     * \t    * ");
+printf("\n\t  \t*\t *\t *\t * ");
+printf("\n\t     *      *\t     *\t    *       * ");
+
+
+
+    printf("\n");
+    return 0;
+}
