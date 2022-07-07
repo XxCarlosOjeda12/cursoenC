b6a72d2c5455f04a164f2fc3b956efc2698c6072
XxCarlosOjeda12 <108698043+XxCarlosOjeda12@users.noreply.github.com>
2022-07-06 19:51:26 -0500

    variables_e2.c
    variables_e2.c

---
+++ variables_e2.c
@@ -0,0 +1,19 @@
+
+#include<stdio.h>
+
+int main(){
+
+int segundos = 400;
+int minutos = segundos/60;   //Division entera
+
+
+printf("\n\t %d segundos son:", segundos);
+
+segundos = segundos%60;
+
+printf(" %d minutos con %d segundos", minutos,segundos);
+
+printf("\n");
+
+    return 0; 
+}
