7220f1425c320547ee8f5ed064277e801db2a0e1
XxCarlosOjeda12 <108698043+XxCarlosOjeda12@users.noreply.github.com>
2022-07-11 17:28:43 -0500

    Add files via upload

---
+++ Clase 2/nombre.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int main(){
+
+    printf("\n\t Hola mundo en lenguaje C");
+
+    return 0;
+}
---
+++ Clase 3/variables_elias.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main(){
+
+int horas= 45;
+int minutos= horas*60  ;
+
+printf("\n\t %d horas son", horas);
+
+
+
+printf(" %d minutos", minutos);
+
+    return 0;
+}
